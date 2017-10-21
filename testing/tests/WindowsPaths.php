137c02dc9920c8544a57dc62290d295e9dd219bf
Spencer Mortensen <spencer@spencermortensen.com>
2017-10-21 18:01:11 -0400

    Added unit tests

--- testing/tests/WindowsPaths.php
+++ testing/tests/WindowsPaths.php
@@ -50,35 +50,164 @@ $output = array(
 
 
 // Test
-$output = $paths->join();
-
-// Output
-$output = null;
-
-
-// Test
-$output = $paths->join($aPath);
+$output= $paths->implode($input);
 
 // Input
-$aPath = 'C:\\';
+$input = array(
+	'drive' => 'C',
+	'atoms' => array(),
+	'isAbsolute' => true
+);
 
 // Output
 $output = 'C:\\';
 
+// Input
+$input = array(
+	'drive' => 'C',
+	'atoms' => array(),
+	'isAbsolute' => false
+);
 
-// Test
-$output = $paths->join($aPath, $bPath);
+// Output
+$output = 'C:';
 
 // Input
-$aPath = 'C:\\';
-$bPath = 'Windows';
+$input = array(
+	'drive' => 'C',
+	'atoms' => array('Windows', 'win.ini'),
+	'isAbsolute' => true
+);
+
+// Output
+$output = 'C:\\Windows\\win.ini';
+
+// Input
+$input = array(
+	'drive' => 'C',
+	'atoms' => array('Windows', 'win.ini'),
+	'isAbsolute' => false
+);
+
+// Output
+$output = 'C:Windows\\win.ini';
+
+
+// Test
+$output = call_user_func_array(array($paths, 'join'), $input);
+
+// Input
+$input = array();
+
+// Output
+$output = null;
+
+// Input
+$input = array('C:\\');
+
+// Output
+$output = 'C:\\';
+
+// Input
+$input = array('C:\\', 'Windows');
 
 // Output
 $output = 'C:\\Windows';
 
 // Input
-$aPath = 'C:';
-$bPath = 'Windows';
+$input = array('C:', 'Windows');
 
 // Output
 $output = 'C:Windows';
+
+
+// Test
+$c = $paths->isChildPath($a, $b);
+
+// Input
+$a = 'C:\\';
+$b = 'C:\\';
+
+// Output
+$c = true;
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\';
+
+// Output
+$c = false;
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Users';
+
+// Output
+$c = false;
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Windows';
+
+// Output
+$c = true;
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Windows\\win.ini';
+
+// Output
+$c = true;
+
+
+// Test
+$c = $paths->getRelativePath($a, $b);
+
+// Input
+$a = 'C:\\';
+$b = 'C:\\';
+
+// Output
+$c = '.';
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\';
+
+// Output
+$c = '..';
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Users';
+
+// Output
+$c = '..\\Users';
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Windows';
+
+// Output
+$c = '.';
+
+// Input
+$a = 'C:\\Windows';
+$b = 'C:\\Windows\\win.ini';
+
+// Output
+$c = 'win.ini';
+
+// Input
+$a = 'C:\\Windows\\system';
+$b = 'C:\\Users\\Public';
+
+// Output
+$c = '..\\..\\Users\\Public';
+
+// Input
+$a = 'C:\\Windows\\system';
+$b = 'C:\\Windows\\system.ini';
+
+// Output
+$c = '..\\system.ini';
